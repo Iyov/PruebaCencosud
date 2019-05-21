eefa19866ff88ae4536d5023436bcf2c9d235c61
Francisco Barrientos <francisco.barrientos@usach.cl>
2019-05-21 00:10:06 -0400

    Ya teniendo el CSV en un DataFrame, se obtiene el campo Location

--- PruebaCencosud.py
+++ PruebaCencosud.py
@@ -9,10 +9,14 @@ sc = SparkContext(conf=conf)
 sqlContext = SQLContext(sc)
 
 def run():
-    ##print("Aca comienzo la lectura del CSV")
-    df = sqlContext.read.csv("/home/francisco/Documentos/PruebaCencosud/csv/retail-food-stores.csv")
-    df.show()
+    ##Primero se determina donde esta el archivo CSV, en este caso es en este path
+    pathCSV = "/home/francisco/Documentos/TestCencosud/PruebaCencosud/csv/retail-food-stores.csv"
 
+    ##Se define la variable del DataFrame llamada df que contiene la lectura del CSV
+    df = sqlContext.read.csv(pathCSV)
+
+    ##Se selecciona la columna Location (15) y se muestra su contenido
+    df.select(df._c14).show()
 
 if __name__ == "__main__":
     run()
