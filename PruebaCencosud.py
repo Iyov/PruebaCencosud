01449c4e514d792618a75677458f04a13752b272
Francisco Barrientos <francisco.barrientos@usach.cl>
2019-05-21 22:59:11 -0400

    Al identificar que el CSV viene con ultima columna aparentemente invalida, se intento modificar para poder leer la columna Location que posee campos en JSON de Latitud y Longitud, lamentablemente despues de varios intentos no se pudo procesar

--- PruebaCencosud.py
+++ PruebaCencosud.py
@@ -10,13 +10,29 @@ sqlContext = SQLContext(sc)
 
 def run():
     ##Primero se determina donde esta el archivo CSV, en este caso es en este path
-    pathCSV = "/home/francisco/Documentos/TestCencosud/PruebaCencosud/csv/retail-food-stores.csv"
+    pathCSV = "/home/francisco/Documentos/TestCencosud/PruebaCencosud/csv/retail-food-stores_Fix2.csv"
 
     ##Se define la variable del DataFrame llamada df que contiene la lectura del CSV
-    df = sqlContext.read.csv(pathCSV)
+    ##Se intenta corregir el CSV modificandolo, aplicando separador por TAB para que al leer el campo Location no se corte el JSON
+    df = sqlContext.read.option("delimiter", "\\t").csv(pathCSV)
+    df.printSchema()
 
     ##Se selecciona la columna Location (15) y se muestra su contenido
-    df.select(df._c14).show()
+    ##Location = df.select(df._c14)
+    ##Location.show()
+    df.registerTempTable("datos")
+    df = sqlContext.sql("SELECT _c14 AS Location FROM datos")
+    
+    ##La columna Location
+    new_df = sqlContext.read.json(df.rdd)
+    new_df.printSchema()
+    
+    ##Escribe el resultado en un JSON para revisar
+    new_df.coalesce(1).write.json("retail-food-stores.json")
+
+    ##Borra la columna Location y escribir archivo resultado ORC
+    ##df.drop(df._c14).coalesce(1).write.format("orc").mode("overwrite").save("retail-food-stores.orc")
+
 
 if __name__ == "__main__":
     run()
