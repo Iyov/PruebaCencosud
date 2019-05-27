d364ed124b48cb6220e2cac5c23b11694bc65755
Francisco Barrientos <francisco.barrientos@usach.cl>
2019-05-26 23:59:43 -0400

    Se termina el ejercicio, se arrojan los resultados en archivos .CSV y .ORC

--- PruebaCencosud.py
+++ PruebaCencosud.py
@@ -1,5 +1,5 @@
 from pyspark import SparkContext, SparkConf, SQLContext
-from pyspark.sql.functions import lit, exp
+from pyspark.sql.functions import *
 import json
 
 appName = 'Prueba Cencosud'
@@ -9,42 +9,25 @@ conf = SparkConf().setAppName(appName).setMaster(master)
 sc = SparkContext(conf=conf)
 sqlContext = SQLContext(sc)
 
-def run():
+def ejercicio():
     print("Comienzo del Ejercicio")
-
+    
     ##Path donde esta el archivo CSV a procesar, aca se puede poner la ruta donde el usuario tenga el archivo
     pathCSV = "/home/francisco/Documentos/TestCencosud/PruebaCencosud/csv/clean_test.csv"
 
     ##Se define la variable del DataFrame llamada df que contiene la lectura del CSV
-    df = sqlContext.read.option("delimiter", "|").csv(pathCSV)
+    df = sqlContext.read.option("delimiter", "|").option("header", "true").csv(pathCSV)
+    
+    ##Obtiene latitude y longitude como JSON y las agrega, Borra la columna Location
+    df = df.withColumn("latitude", get_json_object(df["Location"],"$.latitude"))
+    df = df.withColumn("longitude", get_json_object(df["Location"],"$.longitude"))
+    df = df.drop("Location")
 
-    ##Se obtiene el rdd del CSV para procesarlo
-    rdd = sc.textFile(pathCSV)
-
-    ##Se transforma el rdd y se obtiene la columna Location como un JSON valido
-    rdd2=rdd.map(lambda x: x.split('|')[13])
-    dfLocation = sqlContext.read.json(rdd2)
-
-    ##Escribe la columna Location en un JSON
-    dfLocation.coalesce(1).write.format("json").mode("overwrite").save("resultado.json")
-
-    ##Se obtiene campo latitude del JSON
-    dfLatitude = dfLocation.select("latitude")
-
-    ##Se obtiene campo longitude del JSON
-    dfLongitude = dfLocation.select("longitude")
-
-    ##Borra la columna Location y agrega latitude y longitude en el DataFrame
-    df = df.drop(df._c13)
-    ##df = df.withColumn("latitude", dfLatitude["latitude"])
-    ##df.printSchema()
-    ##df = df.withColumn("longitude", lit(dfLongitude["longitude"]))
-    ##df.printSchema()
-
-    ##Escribe el resultado en archivo ORC
+    ##Escribe el resultado en archivo CSV y ORC
+    df.coalesce(1).write.format("csv").mode("overwrite").save("resultado.csv")
     df.coalesce(1).write.format("orc").mode("overwrite").save("resultado.orc")
 
     print("Fin del Ejercicio")
 
 if __name__ == "__main__":
-    run()
+    ejercicio()
